4a7ce96bffdee292b5dbcb0da037a6909679da7d
italo gabriel <italog03@gmail.com>
2023-11-28 22:30:07 -0300

    add product routes

--- src/infrastructure/api/express.ts
+++ src/infrastructure/api/express.ts
@@ -2,10 +2,13 @@ import express, { Express } from "express";
 import { Sequelize } from "sequelize-typescript";
 import CustomerModel from "../customer/repository/sequelize/customer.model";
 import { customerRoute } from "./routes/customer.routes";
+import { productRoute } from "./routes/product.routes";
+import ProductModel from "../product/repository/sequelize/product.model";
 
 export const app: Express = express();
 app.use(express.json());
 app.use("/customer", customerRoute);
+app.use("/product", productRoute);
 
 export let sequelize: Sequelize;
 
@@ -15,7 +18,7 @@ async function setupDb() {
     storage: ":memory:",
     logging: false,
   });
-  sequelize.addModels([CustomerModel]);
+  sequelize.addModels([CustomerModel, ProductModel]);
   await sequelize.sync();
 }
 setupDb();
